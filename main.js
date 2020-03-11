7f6d2f82fd8899079b1fe137272e31925cf24465
Dimitri Marchetti <dimitri.marchetti@gmail.com>
2020-03-11 03:55:37 -0300

    Classes and style bindings

--- main.js
+++ main.js
@@ -5,6 +5,30 @@ new Vue({
         img: 'assets/vmSocks-green-onWhite.jpg',
         altText: 'A pair of socks',
         inventory: 12,
-        onSale: true
+        inStock:false,
+        onSale: true,
+        details: ["80% Cotton", "20% Polyester", "Gender-neutral"],
+        sizes: [38, 39, 40, 42],
+        cart: 0,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "Green",
+                variantImage: "assets/vmSocks-green-onWhite.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "Blue",
+                variantImage: "assets/vmSocks-blue-onWhite.jpg"
+            },
+        ],
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.img = variantImage
+        }
     }
 })
